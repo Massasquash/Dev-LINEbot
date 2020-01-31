96b6d1785849bbb0b95189763a3fd9e3bba0f7d1
Massasquash <fantasy5512.fun@gmail.com>
2020-01-31 12:25:26 +0900

    Add function of reply URL of Calendar and Sheet, also get both by id for development

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var CHANNEL_ACCESS_TOKEN = "rl+DTHhZ8PyTgSFeeiD30an9Ts6VU1OP1v+VNuYfR1tyAT/muNfhmFgLioDvRND+N25Cma6vE9ijuIHvTasPukgEUefYQFy45rR+L941wbMNoginFfWT/+vA0WDWfddN0r99dHqfZhuFY5mqbffXygdB04t89/1O/w1cDnyilFU=";
 
-var spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+var ssForLogs = SpreadsheetApp.getActiveSpreadsheet();
+var calendar = CalendarApp.getCalendarById("ohsakifarm@gmail.com");
+var spreadsheet = SpreadsheetApp.openById("1wf9QCy-5v5Qi9i2Qiiqg3ZxdLkoR0OHIRyG9N_kmldw");
 
 // メイン処理。LINE botがユーザーからメッセージを受け取った時
 function doPost(e) {
@@ -18,12 +20,16 @@ function getMessage(e){
   var messageText = event.message.text;
   var cache = CacheService.getScriptCache();
 
+  // ユーザーから受け取ったメッセージを部分一致で処理を分岐
   if(messageText.match("おつかれ")){
 
     
 
   }else if(messageText.match("履歴")){
-
+    message1 = "カレンダー\nhttps://calendar.google.com/calendar/embed?src=ohsakifarm%40gmail.com&ctz=Asia%2FTokyo"
+    message2 = "シート\nhttps://docs.google.com/spreadsheets/d/1wf9QCy-5v5Qi9i2Qiiqg3ZxdLkoR0OHIRyG9N_kmldw/edit?usp=sharing"
+    reply(replyToken, message1);
+    reply(replyToken, message2);
 
   }else{
     message = "【READ ME】\n●「おつかれ」と入れてみてください。日報を入力できます。\n●「履歴」と入れるとカレンダー・シートを送ります。";
@@ -64,7 +70,7 @@ function reply(replyToken, message){
 //スプレッドシートにログを表示するためのもの
 function outputLog(text){
   var sheetName = "logs";
-  spreadSheet.getSheetByName(sheetName).appendRow(
+  ssForLogs.getSheetByName(sheetName).appendRow(
     [new Date(), text]
   );
 }
