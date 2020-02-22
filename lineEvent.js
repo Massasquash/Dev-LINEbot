20cee8510c2cc91f9a26666d5dd8851e724fb93a
Massasquash <fantasy5512.fun@gmail.com>
2020-02-22 16:14:23 +0900

    Create Richmenu

--- lineEvent.js
+++ lineEvent.js
@@ -1,9 +1,4 @@
-var richMenuId = "";
-var richMenuUrl = "https://api-data.line.me/v2/bot/richmenu/" + richMenuId;
-var richMenuHeader = {
-  "Content-Type" : "image/jpeg",
-  "Authorization" : "Bearer " + prop.CHANNEL_ACCESS_TOKEN
-}
+
 
 function follow(event, replyToken){
   var msg = "【農作業日誌BOT】\nラインで作業日誌を記録してGoogleカレンダーに登録するアプリ。ストレスなく作業の記録を残せるはず。出先でも登録＆確認できるよ！\n使ってみた感想を教えてね\n\nまずは↓の画像マニュアルに従って、自分用の「作業カテゴリ」を登録してみよう";
@@ -15,19 +10,83 @@ function follow(event, replyToken){
 }
 
 
+function makeMenuObj(){
 
-function postRichMenuImage(){
-
-
-  var content = "";
-  var contentType = "image/png";
-
-  var options = {
-    "method" : "post",
-    "headers" : header,
-    "payload" : DriveApp.getFileById(content).getBlob()
+  var menuObj = {
+    "size":{
+        "width":1200,
+        "height":405
+    },
+    "selected": true,
+    "name": "WorkdiaryMenu",
+    "chatBarText": "農作業日誌",
+    "areas": [{
+      "bounds": {
+          "x": 0,
+          "y": 0,
+          "width": 400,
+          "height": 405
+      },
+      "action": {
+          "type": "text",
+          "text": "おつかれさま！"
+      }
+    },{
+      "bounds": {
+          "x": 400,
+          "y": 0,
+          "width": 400,
+          "height": 405
+      },
+      "action": {
+        "type": "text",
+        "text": "履歴を見る"
+      }
+    },{
+      "bounds": {
+          "x": 800,
+          "y": 0,
+          "width": 400,
+          "height": 405
+      },
+      "action": {
+        "type": "text",
+        "text": "使い方を知りたい"
+      }
+    }]
   };
+}
 
-  var response = UrlFetchup.fetch(richMenuUrl, options);
 
+function postMenuImage(){
+
+  var menuId = "";
+  var menuUrl = "https://api-data.line.me/v2/bot/richmenu/" + menuId;
+  var menuHeader = {
+    "Content-Type" : "image/png",
+    "Authorization" : "Bearer " + prop.CHANNEL_ACCESS_TOKEN
+  }
+
+  var content = "1OmZ6evpPaIECG5zqpIDbK9TcjN07wCBJ";
+   
+  var response = UrlFetchApp.fetch(richMenuUrl, {
+    'headers': menuHeader,  
+    'method': 'post',
+    'payload': DriveApp.getFileById(content).getBlob()
+  });
+}
+
+function setDefoultMenu(){
+
+  var setMenuUrl = "https://api.line.me/v2/bot/user/all/richmenu" + menuId;
+  var menuHeader = {
+    "Authorization" : "Bearer " + prop.CHANNEL_ACCESS_TOKEN
+  }
+
+   
+  var response = UrlFetchApp.fetch(richMenuUrl, {
+    'headers': menuHeader,  
+    'method': 'post',
+    'payload': DriveApp.getFileById(content).getBlob()
+  });
 }
