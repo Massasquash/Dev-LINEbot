0f6ca519e77c0cd005a981c158919635546a6fd3
Massasquash <fantasy5512.fun@gmail.com>
2020-03-10 14:23:08 +0900

    Replace var to let, const

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ var historySheet      = spreadsheet.getSheetByName('作業履歴');
 var userSheet         = spreadsheet.getSheetByName('ユーザー設定');
 var categories        = getCategories('B5:B17');
 
-var eventExp =  /(.*?)\n([\s\S]*)/;
-
 
 function doPost(e) {
   //ポストデータがWEBフォームからのものか、LINEからのものか判定
@@ -30,8 +28,8 @@ function doPost(e) {
     }
   
   //LINE botがユーザーからメッセージを受け取った時の処理
-  var event = JSON.parse(e.postData.contents).events[0];
-  var replyToken = event.replyToken;
+  const event = JSON.parse(e.postData.contents).events[0];
+  const replyToken = event.replyToken;
   if(typeof replyToken === 'undefined'){
     return;
   }
@@ -58,15 +56,15 @@ function doPost(e) {
 // メッセージを受け取った時の処理
 function getMessage(event, replyToken){
   if(event.message.type != "text"){
-    var msg = "／^o^＼";
+    let msg = "／^o^＼";
     reply(replyToken, msg);
     return;
   }
 
-  var messageText = event.message.text;
+  const messageText = event.message.text;
 
-  var cache = CacheService.getScriptCache();
-  var flag = cache.get("flag");
+  const cache = CacheService.getScriptCache();
+  const flag = cache.get("flag");
   
   // ユーザーから受け取ったメッセージにより処理を分岐（メニューから選択）
   if(messageText.match("おつかれさま！")){
@@ -85,9 +83,9 @@ function getMessage(event, replyToken){
   } else {
     // 適当なメッセージに対する処理
     if(flag == null){
-      var msg = "メニューを表示するには左下のボタンをタップしてね ＼(^o^)／";
+      let msg = "メニューを表示するには左下のボタンをタップしてね ＼(^o^)／";
       reply(replyToken, msg);
-      return;
+      return; 
 
     } else {
     //WORK登録処理を進める
@@ -95,43 +93,45 @@ function getMessage(event, replyToken){
         case "1":
           cache.put("flag", 2)
           cache.put("category", event.message.text);
-          var msg1 = "（２）[作業名]を入れてね。\n２行目以降には[作業の詳細]も入れられるよ（無くても問題ないよ）\n（※左下の小さなアイコンをタップしたらキーボードが出るよ！）\n\n↓こんな感じでヨロシク";
-          var msg2 = "[作業名]追肥\n[詳細]圃場●●と××\n[詳細]硫安 20kg/10a";
+          let msg1 = "（２）[作業名]を入れてね。\n２行目以降には[作業の詳細]も入れられるよ（無くても問題ないよ）\n（※左下の小さなアイコンをタップしたらキーボードが出るよ！）\n\n↓こんな感じでヨロシク";
+          let msg2 = "[作業名]追肥\n[詳細]圃場●●と××\n[詳細]硫安 20kg/10a";
           replyMessages(replyToken, msg1, msg2);
           break;
       
         case "2":
           //タイトル・詳細を取得（ユーザー入力により分岐）
           if(categories.indexOf(messageText) < 0 ){
-            if(messageText.match(eventExp)){
-              var [fullText, title, desc] = messageText.match(eventExp);
+            if(messageText.match(/(.*?)\n([\s\S]*)/)){
+              let [fullText, title, desc] = messageText.match(/(.*?)\n([\s\S]*)/);
               //取得文字数を制限
               title = title.substr(0, 20);
               desc = desc.substr(0, 200);
-              cache.put("title", title);
+              cache.putAll({ "title" : title, "desc" : desc});
 
-            } else if(messageText.match(/(.*)/)){
-              title = messageText.substr(0, 20);              
-              cache.put("title", title);
-              var desc = "";
+            } else {
+              // (.*)/
+              const title = messageText.substr(0, 20);              
+              const desc = "";
+              cache.putAll({ "title" : title, "desc" : desc});
             }
           }else {
-              msg = "もう一度[作業名][作業詳細]を入力してね";
+              let msg = "もう一度[作業名][作業詳細]を入力してね";
               reply(replyToken, msg);
               return;
           }
-
-          var [title, date] = createDataForCalender(cache);
-          var [year, month, day] = [date.getFullYear(), date.getMonth()+1, date.getDate()];
-          var displayDate = year + "/" + month + "/" + day;
-
-          var msg = "カレンダーに日報を登録したよ\n◼️日付：${displayDate}\n◼️タイトル：${title}\n◼️詳細：${desc}".replace("${displayDate}", displayDate).replace("${title}", title).replace("${desc}", desc);
+          
+          outputLog("debug", "1", "");
+          const [title, date, desc] = createDataForCalender(cache);
+          const [year, month, day] = [date.getFullYear(), date.getMonth()+1, date.getDate()];
+          const displayDate = year + "/" + month + "/" + day;
+          outputLog("debug", "2", "");
+          let msg = "カレンダーに日報を登録したよ\n◼️日付：${displayDate}\n◼️タイトル：${title}\n◼️詳細：${desc}".replace("${displayDate}", displayDate).replace("${title}", title).replace("${desc}", desc);
 
           // カレンダー・シートへの登録処理。コーディング時はコメントアウト推奨
-          var option = { description: desc };
-          var event = calendar.createAllDayEvent(title, date, option);
+          const option = { description: desc };
+          const postEvent = calendar.createAllDayEvent(title, date, option);
           historySheet.appendRow(
-            [displayDate, cache.get("category"), cache.get("title"), desc, event.getId()]
+            [displayDate, cache.get("category"), cache.get("title"), cache.get("desc"), postEvent.getId()]
           );
           // カレンダー・シートへの登録処理ここまで
 
@@ -147,28 +147,21 @@ function getMessage(event, replyToken){
 
 //ポストバックアクションを受け取った時の処理
 function getPostback(event, replyToken){
-  var readmeAry = ["action=readme00", "action=readme01", "action=readme02",
+  const readmeAry = ["action=readme00", "action=readme01", "action=readme02",
                    "action=readme10", "action=readme11", "action=readme12",
                    "action=readme20", "action=readme21", "action=readme22"];
 
-  var cache = CacheService.getScriptCache();
+  const cache = CacheService.getScriptCache();
+  let date;
+  let msg;
 
-  //日報入力ボタンテンプレートの入力により分岐を処理
   if(event.postback.data == "action=today"){
-    //今日の日付を選んだ場合はdateは"0"を入れる
-    var date = "0";
-    var msg = "（1）今日やった作業の[カテゴリ]を選んでね";
+    date = "0";
+    msg = "（1）今日やった作業の[カテゴリ]を選んでね";
 
   } else if(event.postback.data == "action=settime"){
-    //日時選択アクションで取得した日付はstring型でdateに入る
-    var date = event.postback.params.date;
-    date = date.replace("-", "/").replace("-", "/");
-    var msg = "（1）${date}にやった作業の[カテゴリ]を選んでね".replace("${date}", date);
-  
-  } else if(event.postback.data == "action=editcategory"){
-    var msg = "↓を開いてカテゴリを編集してね\n" + webUrl;
-    reply(replyToken, msg);
-    return;
+    date = event.postback.params.date.replace("-", "/").replace("-", "/");
+    msg = "（1）${date}にやった作業の[カテゴリ]を選んでね".replace("${date}", date);
 
   } else if(event.postback.data == "action=cancel"){
     cache.removeAll(["flag", "date", "category", "title"]);
@@ -176,12 +169,10 @@ function getPostback(event, replyToken){
     reply(replyToken, msg);
     return;
 
-  //使い方カルーセルテンプレートを出す
 } else if(event.postback.data == "action=howto"){
   sendHowtoTemplate(replyToken);
   return; 
 
-  //使い方カルーセルテンプレートの入力により分岐を処理
   } else if(readmeAry.indexOf(event.postback.data) >= 0){
     msg = readmeMessages[readmeAry.indexOf(event.postback.data)][2];
     reply(replyToken, msg);
@@ -196,26 +187,22 @@ function getPostback(event, replyToken){
 
 //Googleカレンダーに登録する情報を作る処理
 function createDataForCalender(cache){
-  var _date = cache.get("date");
-  var _category = cache.get("category");
-  var _title = cache.get("title");
-
+  let [_date, _category, _title, desc] = [cache.get("date"), cache.get("category"), cache.get("title") , cache.get("desc")];
+  title = "[" + _category + "]" + _title
   if(_date == "0"){
-    var date = new Date();
+    date = new Date();
   } else {
-    var date = new Date(_date);
+    date = new Date(_date);
   }
-  var title = "[" + _category + "]" + _title
-
-  return [title, date];
+  return [title, date, desc];
 }
 
 
 //ユーザーカテゴリー取得
 function getCategories(range){
-  var _categories = userSheet.getRange(range).getValues();
-  var categories = [];
-  for(var i in _categories){
+  const _categories = userSheet.getRange(range).getValues();
+  const categories = [];
+  for(let i in _categories){
     if(_categories[i][0] == "") {
       break;
     } else {
